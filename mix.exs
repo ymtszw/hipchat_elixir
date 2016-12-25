1047488e29b254e00d9dff1c514d6e87fc0ce8ed
ymtszw <ymtszw@gmail.com>
2016-12-25 19:30:06 +0900

    Release 0.2.1

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule Hipchat.Mixfile do
   def project do
     [
       app:             :hipchat_elixir,
-      version:         "0.2.0",
+      version:         "0.2.1",
       elixir:          "~> 1.3",
       build_embedded:  Mix.env == :prod,
       start_permanent: false,
