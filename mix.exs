3cb843ea96143ff734bb7ef154d0ce5322222994
ymtszw <ymtszw@gmail.com>
2016-12-30 17:54:22 +0900

    Prepare intermediate release

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule Hipchat.Mixfile do
   def project do
     [
       app:             :hipchat_elixir,
-      version:         "0.2.1",
+      version:         "0.2.2",
       elixir:          "~> 1.3",
       build_embedded:  Mix.env == :prod,
       start_permanent: false,
