bb0f74b514aab4586ef87bb843ab3a693a6d7177
Yu Matsuzawa <yu.matsuzawa@access-company.com>
2018-06-13 17:10:45 +0900

    [0.4.0] Bump version

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule Hipchat.Mixfile do
   def project() do
     [
       app:             :hipchat_elixir,
-      version:         "0.3.0",
+      version:         "0.4.0",
       elixir:          "~> 1.3",
       build_embedded:  Mix.env == :prod,
       start_permanent: false,
