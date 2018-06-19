43511556b0a201cf09d5fc859684bcabb006c7dc
ymtszw <ymtszw@gmail.com>
2018-06-19 21:32:24 +0900

    [master] Bump to 0.5.1, just improve docs

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule Hipchat.Mixfile do
   def project() do
     [
       app:             :hipchat_elixir,
-      version:         "0.5.0",
+      version:         "0.5.1",
       elixir:          "~> 1.3",
       build_embedded:  Mix.env == :prod,
       start_permanent: false,
